b3f080dc799237a7f7f621270a15dbc83f57ad84
Enrico Tassi <gares@fettunta.org>
2012-12-06 14:51:04 +0100

    better test script

--- go.sh
+++ go.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 
 rm -f gmon.out ocamlprof.dump
@@ -13,8 +13,26 @@ build() {
   build_aux $1.cmx $1.ml "ocamlopt -p"
 }
 
+mksanitytest() {
+TEST=/tmp/sanitytest
+cat > $TEST.v <<EOT
+Definition test1 := 
+   (fun (T : Type) (a b : T) (F : T -> T) => 
+     (fun f x y => f y x) (fun x y => F x) a b).
+Definition test2 := 
+   (fun (T : Type) (a b : T) (F G H : T -> T) (pair : T -> T -> T) => 
+     (fun f x y => f (G y) x) (fun x y => pair (F x) a) a (H b)).
+
+Eval $1 in test1.
+Eval $1 in test2.
+Eval $1 in (33 * 4 + 2 - 1).
+
+EOT
+}
+
 mktest() {
-cat > /tmp/test.v <<EOT
+TEST=/tmp/test
+cat > $TEST.v <<EOT
 Fixpoint fact n := match n with 0 => 1 | S m => fact m * n end.
 Let n := $1. 
 Let test := (fact n - fact n).
@@ -23,51 +41,78 @@ Fixpoint fact_s n := match n with 0 => 1 | S m => n * fact_s m end.
 Let n_s := $1.
 Let test_s := (fact_s n_s - fact_s n_s).
 
+Let test3 := n * n * (n * n * n) - (n * n * n) * n.
+Let test4 s o := 
+  (fun (f:nat -> nat) (x : nat) => ( ( (f (f (f (f x)))))))
+   ((fun (f:nat -> nat) (x : nat) => ( ( (f (f (f (f x))))))) s)
+   o.
+Let test6 := (fun x => x - x) (fact_s n_s).
+
 Goal True.
-
-idtac "------------ $2, without sharing --------------".
 Time Eval $2 in test.
-
-idtac "------------- $2, with sharing ---------------".
-idtac "----> $2".
 Time Eval $2 in test_s.
+Time Eval $2 in test3.
+Time Eval $2 in test4.
+Time Eval $2 in (test4 S).
+Time Eval $2 in (test4 S 0).
+Time Eval $2 in test6.
 
 Abort. 
 EOT
 }
+runcoq() {
+OCAMLRUNPARAM='s=33554432,o=120' "$@"
+}
 
-echo compiling with profiling enabled
+print() {
+echo "INFO: $@"
+}
+# -------------------------------------------------------------------
+
+print compiling with profiling enabled
 build kernel/conversion
 build kernel/closure
+build kernel/reduction
 
-echo building Coq
+print building Coq
 make -j2 states bin/coqtop.byte > /tmp/coqlog 2>&1 || \
 	(cat /tmp/coqlog; exit 1)
-grep warning /tmp/coqlog || true
+grep -i warning /tmp/coqlog || true
 
-OPT=9
-BYTE=9
+print 'running *************** sanity test *************'
+mksanitytest lazy
+runcoq bin/coqtop -compile $TEST 2>/dev/null 1>/tmp/t1
+mksanitytest mines
+runcoq bin/coqtop -compile $TEST 2>/dev/null 1>/tmp/t2
+diff /tmp/t1 /tmp/t2
 
-echo 'running *************** opt ' $OPT ' *****************'
-mktest $OPT mine
-OCAMLRUNPARAM='s=33554432,o=120' bin/coqtop -compile /tmp/test
-echo generating gprof.mine.out
-gprof bin/coqtop > gprof.mine.out
+OPT=4
+BYTE=4
+
+print 'running *************** opt ' $OPT ' *****************'
 mktest $OPT lazy
-OCAMLRUNPARAM='s=33554432,o=120' bin/coqtop -compile /tmp/test
-echo generating gprof.lazy.out
+runcoq bin/coqtop -compile $TEST > /tmp/t1
+grep ^Finished /tmp/t1
+print generating gprof.lazy.out
 gprof bin/coqtop > gprof.lazy.out
+mktest $OPT mines
+runcoq bin/coqtop -compile $TEST > /tmp/t2
+grep ^Finished /tmp/t2
+print generating gprof.mine.out
+gprof bin/coqtop > gprof.mine.out
+diff <(grep -v ^Finished /tmp/t1) <(grep -v ^Finished /tmp/t2)
 
-echo 'running *************** byte ' $BYTE ' ******************'
-mktest $BYTE mine
-OCAMLRUNPARAM='s=33554432,o=120' bin/coqtop.byte -compile /tmp/test
-
-echo generating annot.out
-echo '(* vim: set ft=ocaml: *)' > annot.out
-ocamlprof kernel/conversion.ml >> annot.out
-ocamlprof kernel/closure.ml >> annot.out
-
+print 'running *************** byte ' $BYTE ' ******************'
 mktest $BYTE lazy
-OCAMLRUNPARAM='s=33554432,o=120' bin/coqtop.byte -compile /tmp/test
-
-echo type: gvim -o gprof.mine.out annot.out
+runcoq bin/coqtop.byte -compile $TEST > /tmp/t1
+grep ^Finished /tmp/t1
+mktest $BYTE mines
+runcoq bin/coqtop.byte -compile $TEST > /tmp/t2
+grep ^Finished /tmp/t2
+diff <(grep -v ^Finished /tmp/t1) <(grep -v ^Finished /tmp/t2)
+print generating annot.out
+print '(* vim: set ft=ocaml: *)' > annot.out
+ocamlprof kernel/conversion.ml >> annot.out || true
+ocamlprof kernel/closure.ml >> annot.out || true
+ocamlprof kernel/reduction.ml >> annot.out || true
+print "type: gvim -o gprof.mine.out annot.out"
