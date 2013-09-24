f8f57a135687718b0e006dad56814057279e8b62
François Poulain <fpoulain@metrodore.fr>
2013-09-24 16:10:56 +0200

    Adding a toplevel test suite
    Signed-off-by: François Poulain <fpoulain@metrodore.fr>

---
+++ test-suite/interactive/texmacs-toplevel.v
@@ -0,0 +1,44 @@
+Comments 3 (2 + 2) "ciao".
+
+Lemma toto1 : True.
+
+Theorem toto2 : True.
+
+Definition toto3 := 5.
+
+Definition toto4 : nat := 5.
+
+Definition toto5 : nat. exact 3. (* VernacExtend *) Defined.
+
+Fact toto6 : True. exact I. Qed.
+
+Corollary toto7 : True.
+
+Abort All.
+
+Section Toto.
+
+  Variable n : nat.
+  Variables m s : nat.
+
+  Hypothesis npos : n > 0.
+
+End Toto.
+
+Notation "a ++ b" := (a + b).
+
+Notation mynat := nat.
+
+Reserved Notation "a ** b" (at level 30).
+
+Inductive vect (A : Type) : Type :=
+ | Vnil : vect A
+ | Vcons : A -> vect A -> vect A.
+
+Record two : Type := BuilTwo { a : nat; b : vect nat }.
+
+Canonical Structure two_example := BuilTwo 3 (Vnil nat).
+
+Definition xxx := BuilTwo 3 (Vnil nat).
+
+Canonical Structure xxx.
