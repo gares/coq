44cd01acc0654306a28617339f3f05e836943243
Gaetan Gilbert <gaetan.gilbert@ens-lyon.fr>
2017-02-01 17:20:45 +0100

    Add Trunc files, not yet plugged in.
    Trunc is in kernel.mllib so it gets compiled but is not otherwised used.

---
+++ kernel/trunc.ml
@@ -0,0 +1,344 @@
+(************************************************************************)
+(*  v      *   The Coq Proof Assistant  /  The Coq Development Team     *)
+(* <O___,, *   INRIA - CNRS - LIX - LRI - PPS - Copyright 1999-2016     *)
+(*   \VV/  **************************************************************)
+(*    //   *      This file is distributed under the terms of the       *)
+(*         *       GNU Lesser General Public License Version 2.1        *)
+(************************************************************************)
+
+open Pp
+open CErrors
+open Util
+open Names
+
+module Level =
+  struct
+    module Raw = struct
+      type t =
+        | HSet
+        | HInf
+        | Level of int * DirPath.t
+        | Var of int (* De Bruijn index *)
+
+      let compare a b =
+        match a, b with
+        | HSet, HSet -> 0
+        | HSet, _ -> -1
+        | _, HSet -> 1
+        | Level (i1, dp1), Level (i2, dp2) ->
+           if i1 < i2 then -1
+           else if i1 > i2 then 1
+           else DirPath.compare dp1 dp2
+        | Level _, _ -> -1
+        | _, Level _ -> 1
+        | Var n, Var m ->
+           Int.compare n m
+        | Var _, _ -> -1
+        | _, Var _ -> 1
+        | HInf, HInf -> 0
+
+      let equal a b =
+        match a, b with
+        | HSet, HSet | HInf, HInf -> true
+        | Level (n, d), Level (n', d') -> n == n' && d == d'
+        | Var n, Var m -> n == m
+        | _ -> false
+
+      let hcons = function
+        | HSet -> HSet
+        | HInf -> HInf
+        | Level (n, d) as x ->
+           let d' = DirPath.hcons d in
+           if d' == d then x else Level (n, d')
+        | Var _ as x -> x
+
+      let hash =
+        let open Hashset.Combine in
+        function
+        | HSet -> combinesmall 1 0
+        | HInf -> combinesmall 1 1
+        | Var n -> combinesmall 2 n
+        | Level (n, d) -> combinesmall 3 (combine n (DirPath.hash d))
+    end
+
+    type t = {
+        hash : int;
+        data : Raw.t }
+
+    module Self = struct
+      type self = t
+      type t = self
+      type u = unit
+      let eq x y = x.hash == y.hash && Raw.equal x.data y.data
+      let hash x = x.hash
+      let hashcons () x =
+        let data' = Raw.hcons x.data in
+        if x.data == data' then x else { x with data = data' }
+    end
+
+    let hcons : t -> t =
+      let module H = Hashcons.Make(Self) in
+      Hashcons.simple_hcons H.generate H.hcons ()
+
+    let make l = hcons { hash = Raw.hash l; data = l }
+
+    let hash x = x.hash
+
+    let equal x y = x == y
+    let eq = equal
+
+    let compare u v =
+      if u == v then 0
+      else
+        let c = Int.compare u.hash v.hash in
+        if c == 0 then Raw.compare u.data v.data
+        else c
+
+    let hset = make Raw.HSet
+    let hinf = make Raw.HInf
+
+    let is_hset x = equal hset x
+    let is_hinf x = equal hinf x
+    let is_litteral x = is_hset x || is_hinf x
+
+    let of_path d i = make (Raw.Level (i, DirPath.hcons d))
+
+    let var i = make (Raw.Var i)
+
+    let var_index u =
+      match u.data with
+      | Raw.Var i -> Some i
+      | _ -> None
+
+    let apart u v =
+      let open Raw in
+      match u.data, v.data with
+      | HSet, HInf | HInf, HSet -> true
+      | _ -> false
+
+    let leq u v = equal u v || is_hset u || is_hinf v
+
+    let to_string x =
+      match x.data with
+      | Raw.HSet -> "HSet"
+      | Raw.HInf -> "HInf"
+      | Raw.Level (n, d) -> DirPath.to_string d ^ "." ^ string_of_int n
+      | Raw.Var i -> "Var(" ^ string_of_int i ^ ")"
+
+    let pr u = str (to_string u)
+  end
+
+type truncation_level = Level.t
+
+module TMap =
+  struct
+    module M = HMap.Make (Level)
+    include M
+
+    let union l r =
+      merge (fun k l r ->
+          match l, r with
+          | Some _, _ -> l
+          | _, _ -> r) l r
+
+    let subst_union l r =
+      merge (fun k l r ->
+          match l, r with
+          | Some (Some _), _ -> l
+          | Some None, None -> l
+          | _, _ -> r) l r
+
+    let diff ext orig =
+      fold (fun u v acc ->
+          if mem u orig then acc
+          else add u v acc)
+           ext empty
+
+    let pr f m =
+      h 0 (prlist_with_sep fnl (fun (u, v) ->
+                             Level.pr u ++ f v) (bindings m))
+  end
+
+type 'a truncation_map = 'a TMap.t
+
+module TSet =
+  struct
+    include TMap.Set
+
+    module Hset =
+      Hashcons.Make(
+          struct
+            type t = TMap.Set.t
+            type u = Level.t -> Level.t
+            let hashcons huc s =
+	      fold (fun x -> add (huc x)) s empty
+            let eq s s' =
+	      equal s s'
+            let hash = Hashtbl.hash
+          end)
+
+    let hcons =
+      Hashcons.simple_hcons Hset.generate Hset.hcons Level.hcons
+
+    let pr prl s =
+      str"{" ++ prlist_with_sep spc prl (elements s) ++ str"}"
+  end
+
+type truncation_set = TSet.t
+
+module Truncation =
+  struct
+    module M = Hashlist.Make(Level)
+    type t = M.t
+    open M
+
+    let equal (x : t) (y : t) = x == y
+
+    let hash : t -> int = M.hash
+    let rec hcons = function
+      | Nil -> nil
+      | Cons (x, _, l) -> cons x (hcons l)
+
+    let compare x y =
+      if x == y then 0
+      else
+        let hx = hash x and hy = hash y in
+        let c = Int.compare hx hy in
+        if c == 0 then
+          M.compare Level.compare x y
+        else c
+
+    let of_level (l : Level.t) : t = M.tip l
+
+    let pr x =
+      match x with
+      | Cons (u, _, Nil) -> Level.pr u
+      | _ ->
+         str "max(" ++ hov 0
+                           (prlist_with_sep pr_comma Level.pr (to_list x)) ++
+           str")"
+
+    let pr_with f x =
+      match x with
+      | Cons (u, _, Nil) -> f u
+      | _ ->
+         str "max(" ++ hov 0
+                           (prlist_with_sep pr_comma f (to_list x)) ++
+           str")"
+
+    let is_level = function
+      | Cons (l, _, Nil) -> true
+      | _ -> false
+
+    let level = function
+      | Cons (l, _, Nil) -> Some l
+      | _ -> None
+
+    let levels x =
+      M.fold TSet.add x TSet.empty
+
+    let level_mem = M.mem
+
+    let level_rem u v min =
+      match level v with
+      | Some u' -> if Level.equal u u' then min else v
+      | None -> remove u v
+
+    let hset = of_level Level.hset
+    let hinf = of_level Level.hinf
+
+    let is_hset x = equal hset x
+    let is_hinf x = equal hinf x
+
+    let super _ = hinf
+
+    (* Add l to x which does not contain hset/hinf, keeping it sorted w.r.t. Level.compare *)
+    let rec add_level_name l x =
+      match x with
+      | Nil -> of_level l
+      | Cons (l', _, x') ->
+         let c = Level.compare l l' in
+         if c == 0 then x
+         else if c < 0 then cons l x
+         else cons l' (add_level_name l x')
+
+    let add_level l x =
+      if Level.is_hset l then x
+      else if Level.is_hinf l || is_hinf x then hinf
+      else if is_hset x then of_level l
+      else add_level_name l x
+
+    let sort x =
+      fold add_level x nil
+
+    let sup x y =
+      fold add_level x y
+
+    let fold = M.fold
+
+    let exists = M.exists
+    let for_all = M.for_all
+
+    let smartmap = M.smartmap
+  end
+
+type truncation = Truncation.t
+
+(** Substitution *)
+
+type truncation_level_subst = Level.t truncation_map
+
+type truncation_subst = truncation truncation_map
+
+let trunc_level_subst_of f =
+  fun l ->
+  try let u = f l in
+      match Truncation.level u with
+      | None -> l
+      | Some l -> l
+  with Not_found -> l
+
+let empty_trunc_subst = TMap.empty
+let is_empty_trunc_subst = TMap.is_empty
+
+let empty_trunc_level_subst = TMap.empty
+let is_empty_trunc_level_subst = TMap.is_empty
+
+let subst_truncs_level_level subst l =
+  try TMap.find l subst
+  with Not_found -> l
+
+let subst_truncs_level_truncation subst u =
+  let u' = Truncation.smartmap (subst_truncs_level_level subst) u in
+  if u == u' then u
+  else Truncation.sort u'
+
+let subst_truncs_level_truncation_fn subst u =
+  let u' = Truncation.smartmap subst u in
+  if u == u' then u
+  else Truncation.sort u'
+
+type truncation_level_subst_fn = Level.t -> Level.t
+type truncation_subst_fn = truncation_level -> truncation
+
+let make_trunc_subst subst = fun l -> TMap.find l subst
+
+let subst_truncs_truncation fn ul =
+  let subst, nosubst =
+    Truncation.fold (fun u (subst, nosubst) ->
+        try let a' = fn u in
+            (a' :: subst, nosubst)
+        with Not_found -> (subst, u :: nosubst))
+                    ul ([], [])
+  in
+  if CList.is_empty subst then ul
+  else
+    let substs = List.fold_left Truncation.sup Truncation.M.nil subst in
+    List.fold_left (fun acc u -> Truncation.add_level u acc) substs nosubst
+
+(** Printing *)
+let pr_truncation_subst =
+  TMap.pr (fun u -> str" := " ++ Truncation.pr u ++ spc ())
+
+let pr_truncation_level_subst =
+  TMap.pr (fun u -> str" := " ++ Level.pr u ++ spc ())
---
+++ kernel/trunc.mli
@@ -0,0 +1,169 @@
+(************************************************************************)
+(*  v      *   The Coq Proof Assistant  /  The Coq Development Team     *)
+(* <O___,, *   INRIA - CNRS - LIX - LRI - PPS - Copyright 1999-2016     *)
+(*   \VV/  **************************************************************)
+(*    //   *      This file is distributed under the terms of the       *)
+(*         *       GNU Lesser General Public License Version 2.1        *)
+(************************************************************************)
+
+(** Identity complexity. *)
+
+module Level :
+sig
+  type t
+  (** Type of truncation levels. A truncation level is essentially a unique name
+      that will be associated to constraints later on. *)
+
+  val hset : t
+  val hinf : t
+
+  val is_hset : t -> bool
+  val is_hinf : t -> bool
+  val is_litteral : t -> bool
+
+  val equal : t -> t -> bool
+  (** Equality function *)
+
+  val compare : t -> t -> int
+  (** Comparison function *)
+
+  val hash : t -> int
+
+  val of_path : Names.DirPath.t -> int -> t
+  (** Create a new level from a unique identifier and an associated
+      module path. *)
+
+  val pr : t -> Pp.std_ppcmds
+  (** Pretty-printing *)
+
+  val to_string : t -> string
+  (** Debug printing *)
+
+  val var : int -> t
+
+  val var_index : t -> int option
+end
+
+type truncation_level = Level.t
+
+module TSet :
+sig
+  include CSig.SetS with type elt = Level.t
+
+  val hcons : t -> t
+
+  val pr : (Level.t -> Pp.std_ppcmds) -> t -> Pp.std_ppcmds
+end
+
+type truncation_set = TSet.t
+
+module TMap :
+sig
+  include CMap.ExtS with type key = Level.t and module Set := TSet
+
+  val union : 'a t -> 'a t -> 'a t
+  (** [union x y] favors the bindings in the first map. *)
+
+  val diff : 'a t -> 'a t -> 'a t
+  (** [diff x y] removes bindings from x that appear in y (whatever the value). *)
+
+  val subst_union : 'a option t -> 'a option t -> 'a option t
+  (** [subst_union x y] favors the bindings of the first map that are [Some],
+      otherwise takes y's bindings. *)
+
+  val pr : ('a -> Pp.std_ppcmds) -> 'a t -> Pp.std_ppcmds
+  (** Pretty-printing *)
+end
+
+type 'a truncation_map = 'a TMap.t
+
+module Truncation :
+sig
+  type t
+
+  val compare : t -> t -> int
+  (** Comparison function *)
+
+  val equal : t -> t -> bool
+  (** Equality function *)
+
+  val hash : t -> int
+  (** Hash function *)
+
+  val hcons : t -> t
+  (** Hash-consing *)
+
+  val of_level : Level.t -> t
+  (** Create a truncation representing the given level. *)
+
+  val pr : t -> Pp.std_ppcmds
+  (** Pretty-printing *)
+
+  val pr_with : (Level.t -> Pp.std_ppcmds) -> t -> Pp.std_ppcmds
+
+  val is_level : t -> bool
+  (** Test if the truncation is a level or an algebraic truncation. *)
+
+  val level : t -> Level.t option
+  (** Try to get a level out of a truncation, returns [None] if it
+      is an algebraic truncation. *)
+
+  val levels : t -> TSet.t
+  (** Get the levels inside the truncation *)
+
+  val level_mem : Level.t -> t -> bool
+  (** [level_mem l u]: does l appear in u?
+      Not meaningful for l=hset since that's neutral for sup. *)
+
+  val level_rem : Level.t -> t -> t -> t
+  (** [level_rem u v min] removes [u] from [v], resulting in [min]
+      if [v] was exactly [u]. *)
+
+  val sup : t -> t -> t
+  (** The l.u.b. of 2 truncations *)
+
+  val hset : t
+  val hinf : t
+
+  val is_hset : t -> bool
+  val is_hinf : t -> bool
+
+  val fold : (Level.t -> 'a -> 'a) -> t -> 'a -> 'a
+
+  val exists : (Level.t -> bool) -> t -> bool
+  val for_all : (Level.t -> bool) -> t -> bool
+end
+
+type truncation = Truncation.t
+
+(** {6 Substitution} *)
+
+type truncation_subst_fn = truncation_level -> truncation
+type truncation_level_subst_fn = truncation_level -> truncation_level
+
+(** A full substitution, might involve algebraic truncations *)
+type truncation_subst = truncation truncation_map
+type truncation_level_subst = truncation_level truncation_map
+
+val trunc_level_subst_of : truncation_subst_fn -> truncation_level_subst_fn
+
+val empty_trunc_level_subst : truncation_level_subst
+val is_empty_trunc_level_subst : truncation_level_subst -> bool
+
+(** Substitution of truncations. *)
+val subst_truncs_level_level : truncation_level_subst -> truncation_level -> truncation_level
+val subst_truncs_level_truncation : truncation_level_subst -> truncation -> truncation
+val subst_truncs_level_truncation_fn : truncation_level_subst_fn -> truncation -> truncation
+
+(** Level to truncation substitutions. *)
+
+val empty_trunc_subst : truncation_subst
+val is_empty_trunc_subst : truncation_subst -> bool
+val make_trunc_subst : truncation_subst -> truncation_subst_fn
+
+val subst_truncs_truncation : truncation_subst_fn -> truncation -> truncation
+
+(** {6 Pretty-printing of truncations. } *)
+
+val pr_truncation_level_subst : truncation_level_subst -> Pp.std_ppcmds
+val pr_truncation_subst : truncation_subst -> Pp.std_ppcmds
