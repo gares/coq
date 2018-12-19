9315abae564eef6ff07c657e05ac283543368b61
Gaëtan Gilbert <gaetan.gilbert@skyskimmer.net>
2018-12-19 15:35:34 +0100

    Overlays for SProp (wip)

---
+++ dev/ci/user-overlays/08817-sprop.sh
@@ -0,0 +1,40 @@
+if [ "$CI_PULL_REQUEST" = "8817" ] || [ "$CI_BRANCH" = "sprop" ]; then
+    vst_CI_REF=sprop
+    vst_CI_GITURL=https://github.com/SkySkimmer/VST
+
+    aac_tactics_CI_REF=sprop
+    aac_tactics_CI_GITURL=https://github.com/SkySkimmer/aac-tactics
+
+    coq_dpdgraph_CI_REF=sprop
+    coq_dpdgraph_CI_GITURL=https://github.com/SkySkimmer/coq-dpdgraph
+
+    coqhammer_CI_REF=sprop
+    coqhammer_CI_GITURL=https://github.com/SkySkimmer/coqhammer
+
+    elpi_CI_REF=sprop
+    elpi_CI_GITURL=https://github.com/SkySkimmer/coq-elpi
+
+    equations_CI_REF=sprop
+    equations_CI_GITURL=https://github.com/SkySkimmer/Coq-Equations
+
+    ltac2_CI_REF=sprop
+    ltac2_CI_GITURL=https://github.com/SkySkimmer/ltac2
+
+    unicoq_CI_REF=sprop
+    unicoq_CI_GITURL=https://github.com/SkySkimmer/unicoq
+
+    mtac2_CI_REF=sprop
+    mtac2_CI_GITURL=https://github.com/SkySkimmer/mtac2
+
+    paramcoq_CI_REF=sprop
+    paramcoq_CI_GITURL=https://github.com/SkySkimmer/paramcoq
+
+    plugin_tutorial_CI_REF=sprop
+    plugin_tutorial_CI_GITURL=https://github.com/SkySkimmer/plugin_tutorials
+
+    quickchick_CI_REF=sprop
+    quickchick_CI_GITURL=https://github.com/SkySkimmer/QuickChick
+
+    relation_algebra_CI_REF=sprop
+    relation_algebra_CI_GITURL=https://github.com/SkySkimmer/relation-algebra
+fi
