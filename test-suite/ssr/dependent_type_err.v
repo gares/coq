86117db86c07b29440dd35a743da217875c44fb6
Erik Martin-Dorel <erik.martin-dorel@irit.fr>
2019-03-01 19:26:53 +0100

    [ssr] Fix test-suite/ssr/dependent_type_err.v
    This reverts part of commit cc6bb7640b141272f96e3ee23de4888f1a82fb92

--- test-suite/ssr/dependent_type_err.v
+++ test-suite/ssr/dependent_type_err.v
@@ -13,13 +13,8 @@
 Require Import ssreflect.
 Require Import ssrfun ssrbool TestSuite.ssr_mini_mathcomp.
 
-(* no relevant anymore since "[ssr] Enable flag with_evars in pirrel_rewrite"
-
 Lemma ltn_leq_trans : forall n m p : nat, m < n -> n <= p -> m < p.
 move=> n m p Hmn Hnp; rewrite -ltnS.
 Fail rewrite (_ : forall n0 m0 p0 : nat, m0 <= n0 -> n0 < p0 -> m0 < p0).
-(* this does not fail anymore *)
 Fail rewrite leq_ltn_trans.
-(* this fails but not for the expected reason *)
 Admitted.
- *)
