c5d9246f534421737a5ba2c827e69b2cf9a53a87
Enrico Tassi <Enrico.Tassi@Inria.fr>
2021-05-12 22:15:13 +0200

    better test

--- test-suite/bugs/closed/bug_5706.v
+++ test-suite/bugs/closed/bug_5706.v
@@ -3,11 +3,11 @@ From Coq Require Import RelationClasses Relation_Definitions Setoid.
 
 Parameter P : nat -> Prop.
 Parameter f : nat -> nat.
-Parameter foo_eq : forall x, P x -> x = f x.
+Parameter foo_eq : forall x, P x -> P x -> x = f x.
 
 Goal 0 = f 0.
 Proof.
-  rewrite -(foo_eq 0); [ | reflexivity].
+  rewrite -(foo_eq 0); [ | | reflexivity].
 Abort.
 
 
@@ -15,9 +15,9 @@ Parameter R : relation nat.
 Parameter R_equivalence : Equivalence R.
 #[global] Existing Instance R_equivalence.
 
-Parameter foo_R : forall x, P x -> R x (f x).
+Parameter foo_R : forall x, P x -> P x -> R x (f x).
 
 Goal R 0 (f 0).
 Proof.
-  rewrite -(foo_R 0); [ | apply: Equivalence_Reflexive ].
+  rewrite -(foo_R 0); [ | | apply: Equivalence_Reflexive ].
 Abort.
